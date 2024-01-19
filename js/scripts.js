63eecf6209f69bcdd2945c0346e72653039a2c2e
jpansare0404 <152939757+jpansare0404@users.noreply.github.com>
2024-01-19 15:29:36 -0500

    updatemenu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
+function menuToggle(){
+    var x = document.getElementById(myNavtoggle)
+}
 
-
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+    x.className += ' responsive';
+  }
 function menuToggle() {
     var x = document.getElementById('myNavtoggle');
     if (x.className === 'navtoggle') {
